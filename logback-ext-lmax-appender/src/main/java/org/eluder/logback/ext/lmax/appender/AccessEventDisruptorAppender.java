7b5f3f650992ef342df7e323970229899f68030e
Juyoung Lee <uptownlee89@gmail.com>
2017-06-26 14:34:16 +0900

    remove unused logger change groudId enable leveling for cloudwatch logger

--- logback-ext-lmax-appender/src/main/java/org/eluder/logback/ext/lmax/appender/AccessEventDisruptorAppender.java
+++
@@ -1,7 +0,0 @@
-package org.eluder.logback.ext.lmax.appender;
-
-import ch.qos.logback.access.spi.IAccessEvent;
-
-public class AccessEventDisruptorAppender extends DelegatingDisruptorAppender<IAccessEvent> {
-
-}
