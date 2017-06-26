932359a42a164f2c65e42af19409024ecf9a276b
Juyoung Lee <uptownlee89@gmail.com>
2017-06-26 14:37:05 +0900

    remove unused logger change logback, aws sdk version change groudId enable leveling for cloudwatch logger

--- logback-ext-cloudwatch-appender/src/main/java/org/eluder/logback/ext/cloudwatch/appender/CloudWatchAppender.java
+++ logback-ext-cloudwatch-appender/src/main/java/org/eluder/logback/ext/cloudwatch/appender/CloudWatchAppender.java
@@ -1,11 +1,15 @@
 package org.eluder.logback.ext.cloudwatch.appender;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import org.eluder.logback.ext.aws.core.AwsSupport;
 import org.eluder.logback.ext.core.CommonEventAttributes;
 
 public class CloudWatchAppender extends AbstractCloudWatchAppender<ILoggingEvent> {
+
+
+    private Level level = Level.WARN;
 
     public CloudWatchAppender() {
         super();
@@ -28,5 +32,20 @@ public class CloudWatchAppender extends AbstractCloudWatchAppender<ILoggingEvent
                 return event.getTimeStamp();
             }
         };
+    }
+
+    @Override
+    protected void append(ILoggingEvent event) {
+        if(event.getLevel().isGreaterOrEqual(level)) {
+            super.append(event);
+        }
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
     }
 }
